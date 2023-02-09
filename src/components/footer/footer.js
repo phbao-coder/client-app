d2bfa3db8a1aeb1ee199dd632e1f48ced110b533
hbao-newbie <baob1906432@student.ctu.edu.vn>
2023-02-09 16:50:05 +0700

    Footer component

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,5 +1,55 @@
+import { faFacebook, faInstagram, faTiktok, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import { Link } from 'react-router-dom';
+import routes from '~/config/routes';
+
+import classNames from 'classnames/bind';
+import style from './Footer.module.css';
+
+const cx = classNames.bind(style);
+
 function Footer() {
-    return <div>Footer</div>;
+    return (
+        <div>
+            <section className={cx('footer')}>
+                <div className={cx('social')}>
+                    <a href="http://facebook.com">
+                        <FontAwesomeIcon icon={faFacebook} />
+                    </a>
+                    <a href="http://facebook.com">
+                        <FontAwesomeIcon icon={faInstagram} />
+                    </a>
+                    <a href="http://facebook.com">
+                        <FontAwesomeIcon icon={faTwitter} />
+                    </a>
+                    <a href="http://facebook.com">
+                        <FontAwesomeIcon icon={faTiktok} />
+                    </a>
+                </div>
+
+                <ul className={cx('list')}>
+                    <li>
+                        <Link to={routes.home}>Home</Link>
+                    </li>
+                    <li>
+                        <Link to={routes.about}>About</Link>
+                    </li>
+                    <li>
+                        <Link to={routes.about}>Term</Link>
+                    </li>
+                    <li>
+                        <Link to={routes.about}>Service</Link>
+                    </li>
+                    <li>
+                        <Link to={routes.about}>Privacy policy</Link>
+                    </li>
+                </ul>
+
+                <p className={cx('copyright')}>didong @ 2023</p>
+            </section>
+        </div>
+    );
 }
 
 export default Footer;
