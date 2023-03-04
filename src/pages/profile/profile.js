e0cecfd76c1f2d821437817f7044527b9ca4e294
hbao-newbie <baob1906432@student.ctu.edu.vn>
2023-03-05 00:59:21 +0700

    tang dan/ giam dan

--- src/pages/profile/profile.js
+++ src/pages/profile/profile.js
@@ -1,6 +1,7 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import Select from 'react-select';
 
 import { getOrdersByUser } from '~/store/orders/orderState';
 
@@ -12,12 +13,55 @@ import OrderCard from '~/components/OrderCard/OrderCard';
 
 const cx = classNames.bind(style);
 
+const menu = [
+    { value: 'Tăng dần', label: 'Tăng dần' },
+    { value: 'Giảm dần', label: 'Giảm dần' },
+];
+
+const ascending = (orders) => {
+    let ordersTemp = [...orders];
+    for (let i = 0; i < ordersTemp.length - 1; i++) {
+        for (let j = i + 1; j < ordersTemp.length; j++) {
+            if (ordersTemp[i].paymentIntent.amount > ordersTemp[j].paymentIntent.amount) {
+                let temp = ordersTemp[i];
+                ordersTemp[i] = ordersTemp[j];
+                ordersTemp[j] = temp;
+            }
+        }
+    }
+    return ordersTemp;
+};
+
+const decrease = (orders) => {
+    let ordersTemp = [...orders];
+    for (let i = 0; i < ordersTemp.length - 1; i++) {
+        for (let j = i + 1; j < ordersTemp.length; j++) {
+            if (ordersTemp[i].paymentIntent.amount < ordersTemp[j].paymentIntent.amount) {
+                let temp = ordersTemp[i];
+                ordersTemp[i] = ordersTemp[j];
+                ordersTemp[j] = temp;
+            }
+        }
+    }
+    return ordersTemp;
+};
+
 function Profile() {
     const user = useSelector((state) => state.user.user);
     const isUser = useSelector((state) => state.user.isUser);
     const orders = useSelector((state) => state?.orders?.orders);
     const navigate = useNavigate();
     const dispatch = useDispatch();
+
+    const [ordersRender, setOrderRender] = useState(orders);
+
+    const handleSort = (e) => {
+        if (e.value === 'Tăng dần') {
+            setOrderRender(ascending(orders));
+        } else {
+            setOrderRender(decrease(orders));
+        }
+    };
 
     useEffect(() => {
         if (isUser === false) {
@@ -31,11 +75,20 @@ function Profile() {
 
     return (
         <div className={cx('container')}>
-            <ProfileCard info={user} orders={orders} />
+            <div className={cx('profile')}>
+                <ProfileCard info={user} orders={orders} />
+            </div>
             <div className={cx('orders')}>
-                <h1>Danh sách giỏ hàng</h1>
+                <div className={cx('action')}>
+                    <h1>Danh sách giỏ hàng</h1>
+                    <Select
+                        onChange={(e) => handleSort(e)}
+                        placeholder="Sắp xếp"
+                        options={menu}
+                    ></Select>
+                </div>
                 <div className={cx('order')}>
-                    {orders?.map((order, index) => (
+                    {ordersRender?.map((order, index) => (
                         <OrderCard key={index} order={order} />
                     ))}
                 </div>
