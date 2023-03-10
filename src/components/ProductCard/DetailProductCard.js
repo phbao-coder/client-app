7db4b3fd682b2b253c76ff5caafa1c6c55a796c7
hbao-newbie <baob1906432@student.ctu.edu.vn>
2023-03-10 23:13:05 +0700

    update detail product card

--- src/components/ProductCard/DetailProductCard.js
+++ src/components/ProductCard/DetailProductCard.js
@@ -1,11 +1,17 @@
+import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { addProductToCart, updateIncreaProductInCart } from '~/store/cart/cartState';
 
+import Tippy from '@tippyjs/react/headless';
+
 import vnd from '~/utils/vnd';
+import color from '~/utils/color';
 import addToCart from '~/utils/addToCart';
 
 import classNames from 'classnames/bind';
 import style from './DetailProductCard.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 
 const cx = classNames.bind(style);
 
@@ -29,11 +35,17 @@ function DetailProductCard({ product }) {
                 </div>
             </div>
             <div className={cx('right')}>
-                <h3>{product.name}</h3>
+                <h3>
+                    {product.name} <span>-15%</span>
+                </h3>
                 <h4>{vnd(product.price)} VND</h4>
+                <h4 className={cx('price-sale')}>
+                    {vnd(product.price - (product.price / 100) * 15)} VND
+                </h4>
                 {product.category === 'accessories' && (
                     <p>
                         Chức năng: {product.description} <br />
+                        <span>Màu sắc: {color(product.color)}</span>
                     </p>
                 )}
                 {product.category === 'mobile' && (
@@ -50,7 +62,11 @@ function DetailProductCard({ product }) {
                         <span>Màu sắc: {product.color}</span>
                     </p>
                 )}
-                <button onClick={() => handleAddToCart(product)}>Thêm vào giỏ hàng</button>
+                <Tippy arrow={true} render={() => <></>}>
+                    <button onClick={() => handleAddToCart(product)}>
+                        <FontAwesomeIcon icon={faCartShopping} />
+                    </button>
+                </Tippy>
             </div>
         </div>
     );
