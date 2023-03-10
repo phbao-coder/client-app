b357ee76f363240e36e28af54da3694a4014831f
hbao-newbie <baob1906432@student.ctu.edu.vn>
2023-03-10 14:18:07 +0700

    page not found

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -1,5 +1,31 @@
+import {
+    faAnchorCircleExclamation,
+    faChevronCircleLeft,
+    faHome,
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import classNames from 'classnames/bind';
+import { Link } from 'react-router-dom';
+import style from './NotFound.module.css';
+
+const cx = classNames.bind(style);
+
 function NotFound() {
-    return <div></div>;
+    return (
+        <div className={cx('container')}>
+            <div>
+                <Link to="/">
+                    <FontAwesomeIcon icon={faHome} className={cx('icon')} />
+                </Link>
+            </div>
+            <h1>
+                Không tìm thấy trang <FontAwesomeIcon icon={faAnchorCircleExclamation} />
+                <span>
+                    <FontAwesomeIcon icon={faChevronCircleLeft} /> Trở về trang chủ
+                </span>
+            </h1>
+        </div>
+    );
 }
 
 export default NotFound;
