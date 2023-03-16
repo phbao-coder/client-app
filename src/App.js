32eb359e417a03c359fc7a3589488db5fe86299d
hbao-newbie <baob1906432@student.ctu.edu.vn>
2023-03-16 18:05:59 +0700

    rename files header, footer, product

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { Routes, Route } from 'react-router-dom';
 
 import routers from './routes';
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
+import Header from './components/header/header';
+import Footer from './components/footer/footer';
 
 import './App.css';
 
