5204c12a8ddb363a756c391494b069a34a1b0a02
hbao-newbie <baob1906432@student.ctu.edu.vn>
2023-03-25 12:56:04 +0700

    update header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,13 +1,15 @@
 import { useEffect, useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { userLogout } from '~/store/user/userState';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars, faMobile, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faSearch, faXmark } from '@fortawesome/free-solid-svg-icons';
+
+import { userLogout } from '~/store/user/userState';
 
 import SearchBox from '../SearchBox/SearchBox';
-
 import routes from '~/config/routes';
+import icon_cart from '~/assets/icons/cart_icon.png';
+
 import classNames from 'classnames/bind';
 import style from './Header.module.css';
 
@@ -15,13 +17,13 @@ const cx = classNames.bind(style);
 
 function Header() {
     const user = useSelector((state) => state.user.user);
-    const isUser = useSelector((state) => state.user.isUser);
     const cart = useSelector((state) => state.cart.cart);
+    const isUser = useSelector((state) => state.user.isUser);
     const dispatch = useDispatch();
-    const navigate = useNavigate();
 
-    const [activeMenu, setActiveMenu] = useState({ active: false });
     const [sumCountCart, setSumCountCart] = useState(0);
+    const [displaySearchBox, setDisplaySearchBox] = useState(false);
+    const [displayMenuCenter, setDisplayMenuCenter] = useState({ active: false });
 
     useEffect(() => {
         setSumCountCart(
@@ -33,87 +35,112 @@ function Header() {
 
     const handleLogout = () => {
         dispatch(userLogout());
-        navigate('/login');
     };
 
-    const handleActiveMenu = () => {
-        setActiveMenu((prev) => {
-            return {
-                active: !prev.active,
-            };
-        });
+    const handleDisplaySearchBox = () => {
+        if (displaySearchBox) {
+            setDisplaySearchBox(false);
+            document.body.style.overflow = '';
+        } else {
+            setDisplaySearchBox(true);
+            document.body.style.overflow = 'hidden';
+        }
+    };
+
+    const handleDisplayMenuCenter = () => {
+        setDisplayMenuCenter((prev) => ({ active: !prev.active }));
+        console.log(displayMenuCenter);
     };
 
     return (
-        <header className={cx('header')}>
-            <div className={cx('logo')}>
-                <Link to={routes.home}>
-                    <FontAwesomeIcon icon={faMobile} /> <span>Garden's phone</span>
-                </Link>
-            </div>
-            <SearchBox />
-            <nav className={cx('nav', activeMenu)}>
+        <>
+            <header>
                 <div className={cx('logo')}>
-                    <Link to={routes.home}>
-                        <FontAwesomeIcon icon={faMobile} />
-                        Garden's phone
+                    <Link to={routes.home} className={cx('logo__item')}>
+                        Garden's Phone
                     </Link>
                 </div>
-                <ul>
-                    <li>
-                        <Link to={routes.home} onClick={handleActiveMenu}>
-                            Trang chủ
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to={routes.product} onClick={handleActiveMenu}>
-                            Sản phẩm
-                        </Link>
-                    </li>
-                    <li>
-                        {isUser && (
-                            <Link to={routes.cart} onClick={handleActiveMenu}>
-                                Giỏ hàng{' '}
-                                {sumCountCart !== 0 && (
-                                    <span className={cx('number')}>{sumCountCart}</span>
+                <div className={cx('menu')}>
+                    <div className={cx('menu__container')}>
+                        <div className={cx('menu__item--left')}>
+                            <div className={cx('menu__item--cart')}>
+                                <span>
+                                    <FontAwesomeIcon
+                                        icon={faBars}
+                                        className={cx('menu__item--cart--icon')}
+                                        onClick={handleDisplayMenuCenter}
+                                    />
+                                </span>
+                                <Link to={routes.cart}>
+                                    <img
+                                        src={icon_cart}
+                                        alt=""
+                                        className={cx('menu__item__cart--icon')}
+                                    />
+                                    <span className={cx('menu__item__cart--number')}>
+                                        {sumCountCart}
+                                    </span>
+                                </Link>
+                            </div>
+                        </div>
+                        <div className={cx('menu__item--center', displayMenuCenter)}>
+                            <nav>
+                                <Link to={routes.home}>Trang chủ</Link>
+                                <Link to={routes.product}>Sản phẩm</Link>
+                                <Link to={routes.about}>Liên hệ</Link>
+                                <FontAwesomeIcon
+                                    icon={faXmark}
+                                    className={cx('menu__item--center--icon')}
+                                    onClick={handleDisplayMenuCenter}
+                                />
+                            </nav>
+                        </div>
+                        <div className={cx('menu__item--right')}>
+                            <div
+                                className={cx('menu__item__search')}
+                                onClick={handleDisplaySearchBox}
+                            >
+                                <FontAwesomeIcon
+                                    icon={faSearch}
+                                    className={cx('menu__item__search--icon')}
+                                />
+                            </div>
+                            <div className={cx('menu__item__user')}>
+                                {isUser ? (
+                                    <div>
+                                        <Link to={routes.profile}>{user?.username}</Link>
+                                        <Link to={routes.home} onClick={handleLogout}>
+                                            Đăng xuất
+                                        </Link>
+                                    </div>
+                                ) : (
+                                    <div>
+                                        <Link to={routes.login}>Đăng nhập</Link>
+                                        <Link to={routes.register}>Đăng ký</Link>
+                                    </div>
                                 )}
-                            </Link>
-                        )}
-                    </li>
-                    <li>
-                        <Link to={routes.about} onClick={handleActiveMenu}>
-                            Liên hệ
-                        </Link>
-                    </li>
-                    <li>
-                        {isUser ? (
-                            <Link to={routes.profile} onClick={handleActiveMenu}>
-                                <FontAwesomeIcon icon={faUser} className={cx('icon')} />{' '}
-                                {user.username}
-                            </Link>
-                        ) : (
-                            <Link to={routes.login} onClick={handleActiveMenu}>
-                                <FontAwesomeIcon icon={faUser} className={cx('icon')} /> Đăng nhập
-                            </Link>
-                        )}
-                    </li>
-                    <li>
-                        {isUser ? (
-                            <button onClick={handleLogout} className={cx('button-logout')}>
-                                Đăng xuất
-                            </button>
-                        ) : (
-                            <Link to={routes.register} onClick={handleActiveMenu}>
-                                Đăng ký
-                            </Link>
-                        )}
-                    </li>
-                </ul>
-            </nav>
-            <div className={cx('hamburger')} onClick={handleActiveMenu}>
-                <FontAwesomeIcon icon={faBars} />
-            </div>
-        </header>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {displaySearchBox && (
+                    <div className={cx('search__box')}>
+                        <div className={cx('search__box--container')}>
+                            <div className={cx('search__box--container--header')}>
+                                <FontAwesomeIcon
+                                    icon={faXmark}
+                                    className={cx('search__box--container--header--icon')}
+                                    onClick={handleDisplaySearchBox}
+                                />
+                            </div>
+                            <div className={cx('search__box--container--body')}>
+                                <SearchBox />
+                            </div>
+                        </div>
+                    </div>
+                )}
+            </header>
+        </>
     );
 }
 
