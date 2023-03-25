360e6342af453f20df733119c900647e9b358e10
hbao-newbie <baob1906432@student.ctu.edu.vn>
2023-03-25 15:18:48 +0700

    update css search box

--- src/components/SearchBox/SearchBox.js
+++ src/components/SearchBox/SearchBox.js
@@ -5,8 +5,6 @@ import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
-import Tippy from '@tippyjs/react/headless';
-
 import { getProductsSearch } from '~/store/products/productsState';
 
 import routes from '~/config/routes';
@@ -27,54 +25,53 @@ function SearchBox() {
     }, [value, dispatch]);
 
     return (
-        <div>
-            <Tippy
-                trigger="click"
-                delay={[0, 0]}
-                interactive={true}
-                placement="bottom"
-                offset={[0, 20]}
-                render={(attrs) => (
-                    <div tabIndex="-1" {...attrs} className={cx('box-items')}>
-                        <h2>Sản phẩm gợi ý</h2>
-                        {products.length === 0 ? (
-                            <span className={cx('product-not-found')}>Không tìm thấy sản phẩm</span>
-                        ) : (
-                            <ul>
-                                {products?.map((product) => (
-                                    <li key={product._id} className={cx('item-search')}>
-                                        <div className={cx('left')}>
-                                            <img src={product.images} alt={product.name} />
-                                        </div>
-                                        <div className={cx('right')}>
-                                            <Link
-                                                to={`${routes.product}/${product._id}`}
-                                                className={cx('name')}
-                                            >
-                                                {product.name}
-                                            </Link>
-                                            <p className={cx('price')}>{vnd(product.price)} VND</p>
-                                        </div>
-                                    </li>
-                                ))}
-                            </ul>
-                        )}
-                    </div>
-                )}
-            >
-                <div className={cx('container')}>
-                    <input
-                        type="text"
-                        placeholder="Nhập tên sản phẩm mà bạn cần tìm..."
-                        onChange={(e) => setValue(e.target.value)}
-                        spellCheck={false}
-                    />
-                    <span>
-                        <FontAwesomeIcon icon={faSearch} />
-                    </span>
+        <>
+            <div className={cx('container')}>
+                <input
+                    type="text"
+                    placeholder="Nhập tên sản phẩm mà bạn cần tìm..."
+                    onChange={(e) => setValue(e.target.value)}
+                    spellCheck={false}
+                />
+                <span>
+                    <FontAwesomeIcon icon={faSearch} />
+                </span>
+            </div>
+            {products.length !== 0 && (
+                <div className={cx('box')}>
+                    <h2>Sản phẩm gợi ý</h2>
+                    <ul className={cx('box--container')}>
+                        {products?.map((product) => (
+                            <li key={product._id} className={cx('box__item')}>
+                                <div className={cx('box__item--left')}>
+                                    <img src={product.images} alt={product.name} />
+                                </div>
+                                <div className={cx('box__item--right')}>
+                                    <Link
+                                        to={`${routes.product}/${product._id}`}
+                                        className={cx('box__item--right--name')}
+                                    >
+                                        {product.name}
+                                    </Link>
+                                    <p className={cx('box__item--right--price')}>
+                                        <span className={cx('box__item--right--price--price')}>
+                                            {vnd(
+                                                product.price -
+                                                    (product.price / 100) *
+                                                        product.sale.salePercentage,
+                                            )}
+                                        </span>
+                                        <span className={cx('box__item--right--price--sale')}>
+                                            {vnd(product.price)}{' '}
+                                        </span>
+                                    </p>
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
                 </div>
-            </Tippy>
-        </div>
+            )}
+        </>
     );
 }
 
