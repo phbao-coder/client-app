6366efdebde018367cea90e4108e127b6232aa47
hbao-newbie <baob1906432@student.ctu.edu.vn>
2023-03-26 14:45:25 +0700

    fix when change path, it's no scroll to top

--- src/index.js
+++ src/index.js
@@ -6,12 +6,14 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persitor } from './store/store';
+import ScrollToTop from './components/ScrollToTop/ScrollTotop';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <Provider store={store}>
         <PersistGate loading={null} persistor={persitor}>
             <Router>
+                <ScrollToTop />
                 <App />
             </Router>
         </PersistGate>
