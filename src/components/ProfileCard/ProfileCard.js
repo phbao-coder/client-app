a5475a35ae3b48cf48f882d29359a088ed50e149
hbao-newbie <baob1906432@student.ctu.edu.vn>
2023-03-26 16:39:34 +0700

    update profile card

--- src/components/ProfileCard/ProfileCard.js
+++ src/components/ProfileCard/ProfileCard.js
@@ -1,3 +1,6 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPenToSquare } from '@fortawesome/free-solid-svg-icons';
+
 import vnd from '~/utils/vnd';
 import classNames from 'classnames/bind';
 import style from './ProfileCard.module.css';
@@ -9,33 +12,59 @@ function ProfileCard({ info, orders }) {
     const arrCost = ordersNotCancelled?.map((item) => item.paymentIntent.amount);
     const sumCost = arrCost?.reduce((value, curr) => value + curr, 0);
 
+    const { name, address, phone, username, email } = info;
+
     return (
-        <div className={cx('card')}>
-            <div className={cx('card-image')}>
-                <img
-                    src="https://znews-photo.zingcdn.me/w660/Uploaded/qfssu/2022_05_20/hansara.official_91565310_844475592737706_3368894882940378725_n.jpg"
-                    alt="avt"
-                />
+        <div className={cx('container')}>
+            <div className={cx('container--left')}>
+                <div
+                    className={cx('container__avatar')}
+                    style={{
+                        backgroundImage: `url('https://media.thieunien.vn/thumb//uploads/2021/11/18/han-sara-sinh-nhat-tuoi-21-viet-tam-thu-no-luc-de-truong-thanh-tu-nhung-dieu-chua-hoan-hao_48097.jpg')`,
+                    }}
+                ></div>
             </div>
-            <div className={cx('card-info')}>
-                <div className={cx('card-info-address')}>
-                    <h2>{info.name}</h2>
-                    <p>
-                        {info.address} <br /> {info.email} <br /> {info.phone}
-                    </p>
+            <div className={cx('container--right')}>
+                <div className={cx('container__information')}>
+                    <table>
+                        <tbody>
+                            <tr>
+                                <th>Tên:</th>
+                                <th className={cx('container__information--name')}>{name}</th>
+                            </tr>
+                            <tr>
+                                <th>Username:</th>
+                                <th>{username}</th>
+                            </tr>
+                            <tr>
+                                <th>Email:</th>
+                                <th>{email}</th>
+                            </tr>
+                            <tr>
+                                <th>Số điện thoại:</th>
+                                <th>{phone}</th>
+                            </tr>
+                            <tr>
+                                <th>Địa chỉ: </th>
+                                <th>{address}</th>
+                            </tr>
+                            <tr>
+                                <th>Số đơn: </th>
+                                <th>{arrCost.length}</th>
+                            </tr>
+                            <tr>
+                                <th>Chi: </th>
+                                <th>{vnd(sumCost)} VND</th>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
-                <div className={cx('card-info-order')}>
-                    <div className={cx('left')}>
-                        <h3>{ordersNotCancelled?.length}</h3>
-                        <p>Tổng số đơn</p>
-                    </div>
-                    <div className={cx('right')}>
-                        <h3>{vnd(sumCost)} VND</h3>
-                        <p>Chi phí</p>
-                    </div>
+                <div className={cx('container__edit')}>
+                    <button className={cx('container__edit--button')}>
+                        Chỉnh sửa thông tin <FontAwesomeIcon icon={faPenToSquare} />
+                    </button>
                 </div>
             </div>
-            <button>Cập nhật</button>
         </div>
     );
 }
