804f50d7bd608e20e6d94d91a2dfc582090d27b4
hbao-newbie <baob1906432@student.ctu.edu.vn>
2023-03-26 12:44:43 +0700

    update detail product card and change font

--- src/components/ProductCard/DetailProductCard.js
+++ src/components/ProductCard/DetailProductCard.js
@@ -1,16 +1,15 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { addProductToCart, updateIncreaProductInCart } from '~/store/cart/cartState';
 
-import Tippy from '@tippyjs/react/headless';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 
 import vnd from '~/utils/vnd';
-import colorVietnamese from '~/utils/color';
 import addToCart from '~/utils/addToCart';
+import colorVietnamese from '~/utils/color';
 
 import classNames from 'classnames/bind';
 import style from './DetailProductCard.module.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 
 const cx = classNames.bind(style);
 
@@ -42,45 +41,71 @@ function DetailProductCard({ product }) {
     } = product;
 
     return (
-        <div className={cx('container', 'flex')}>
-            <div className={cx('left')}>
-                <div className={cx('main-image')}>
-                    <img src={images} alt={name} className={cx('slide')} />
-                </div>
+        <div className={cx('container')}>
+            <div className={cx('container--left')}>
+                <img src={images} alt={name} />
             </div>
-            <div className={cx('right')}>
-                <h3>
-                    {name} {sale?.isOnSale && <span>{sale?.salePercentage} %</span>}
-                </h3>
-                <h4>{vnd(price)} VND</h4>
-                <h4 className={cx('price-sale')}>
-                    {vnd(price - (price / 100) * sale?.salePercentage)} VND
-                </h4>
+            <div className={cx('container--right')}>
                 {category === 'accessories' && (
-                    <p>
-                        Chức năng: {description} <br />
-                        <span>Màu sắc: {colorVietnamese(color)}</span>
-                    </p>
+                    <div className={cx('container__information')}>
+                        <h2 className={cx('container__information--name')}>{name}</h2>
+                        <h2 className={cx('container__information--sale')}>
+                            Giảm -{sale.salePercentage}%
+                        </h2>
+                        <div className={cx('container__information--price')}>
+                            <h3>{vnd(price - (price / 100) * sale.salePercentage)}</h3>
+                            <h3 className={cx('container__information--price--original')}>
+                                {vnd(price)}
+                            </h3>
+                        </div>
+                        <button
+                            className={cx('container--right__button')}
+                            onClick={() => handleAddToCart(product)}
+                        >
+                            Thêm vào giỏ hàng <FontAwesomeIcon icon={faCartShopping} />
+                        </button>
+                        <div className={cx('container__information--detail')}>
+                            <h3>Thông số kỹ thuật</h3>
+                            <ul>
+                                <li>Chức năng: {description}</li>
+                                <li>Màu sắc: {colorVietnamese(color)}</li>
+                            </ul>
+                        </div>
+                    </div>
                 )}
                 {category === 'mobile' && (
-                    <p>
-                        Màn hình: {description} <br />
-                        Camera sau: {camBack} <br />
-                        Camera trước: {camFront} <br />
-                        Ram: {ram} GB
-                        <br />
-                        Rom: {memory} GB <br />
-                        OS: {os} <br />
-                        Pin: {pin} mAh
-                        <br />
-                        <span>Màu sắc: {color}</span>
-                    </p>
+                    <div className={cx('container__information')}>
+                        <h2 className={cx('container__information--name')}>{name}</h2>
+                        <h2 className={cx('container__information--sale')}>
+                            Giảm -{sale.salePercentage}%
+                        </h2>
+                        <div className={cx('container__information--price')}>
+                            <h3>{vnd(price - (price / 100) * sale.salePercentage)}</h3>
+                            <h3 className={cx('container__information--price--original')}>
+                                {vnd(price)}
+                            </h3>
+                        </div>
+                        <button
+                            className={cx('container--right__button')}
+                            onClick={() => handleAddToCart(product)}
+                        >
+                            Thêm vào giỏ hàng <FontAwesomeIcon icon={faCartShopping} />
+                        </button>
+                        <div className={cx('container__information--detail')}>
+                            <h3>Thông số kỹ thuật</h3>
+                            <ul>
+                                <li>Màn hình: {description}</li>
+                                <li>Camera trước: {camFront}</li>
+                                <li>Camera sau: {camBack}</li>
+                                <li>Dung lượng RAM: {ram} GB</li>
+                                <li>Dung lượng ROM: {memory} GB</li>
+                                <li>Hệ điều hành: {os}</li>
+                                <li>Dung lượng pin: {pin} mAh</li>
+                                <li>Màu sắc: {colorVietnamese(color)}</li>
+                            </ul>
+                        </div>
+                    </div>
                 )}
-                <Tippy arrow={true} render={() => <></>}>
-                    <button onClick={() => handleAddToCart(product)}>
-                        <FontAwesomeIcon icon={faCartShopping} />
-                    </button>
-                </Tippy>
             </div>
         </div>
     );
